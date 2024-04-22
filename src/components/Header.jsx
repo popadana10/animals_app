0e07a51ba668f0586ded13a2aa354d43e7477600
Dana Popa <popa.dana100@gmail.com>
2024-04-22 13:06:47 +0300

    buttons and images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
     return (
       <header className="header">
         <h1>Zoo app</h1>
+        <span className="material-symbols-outlined">search</span>
       </header>
     );
   };
