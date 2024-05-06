fc0baa5d3cd190495a1611eb5c701de07ffbfe18
Dana Popa <popa.dana100@gmail.com>
2024-05-06 10:57:47 +0300

    dynamic category page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
+import {Link, NavLink} from "react-router-dom";
+
 const Header = () => {
     return (
       <header className="header">
         <h1>Zoo app</h1>
         <span className="material-symbols-outlined">search</span>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/'>Home</NavLink>
+              <NavLink to='/animals'>Animals</NavLink>
+              <NavLink to='/birds'>Birds</NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   };
---
+++ src/routes/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <>
+              <Link to='/animals' >Animals</Link>
+              <Link to='/birds'>Birds</Link>
+        </>
+    );
+}
+export default Home;
+
+
