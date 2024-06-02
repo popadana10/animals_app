130b72edfdf36f121cc339dedbc8e88986811357
Dana Popa <popa.dana100@gmail.com>
2024-06-02 21:51:31 +0300

    Add information about the project on the about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
-    return (
-      <footer>
-        <p>Copyright &copy; Dana Popa</p>
-      </footer>
-    );
-  };
-  export default Footer;
+  return (
+    <footer>
+      <p> &copy; Dana Popa</p>
+    </footer>
+  );
+};
+export default Footer;
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,9 +1,19 @@
-const About = () => {
+const AboutPage = () => {
     return (
-        <>
-        <h2>About will be here.</h2>
-        
-        </>
+        <div id="AboutPage">
+            <h2>About</h2>
+            <p>
+                In this React application, I've curated a collection of animal profiles. Users can interact by liking or removing animals, and there's a convenient search feature for finding specific ones. It's a comprehensive way to explore various creatures from the comfort of your browser. The animals are neatly categorized, making navigation intuitive. Clicking on any animal reveals detailed information, complemented by a handy back button for easy browsing.
+                What sets this app apart is the integration of Wikipedia excerpts, providing enriching insights into each animal. It's a blend of entertainment and education!
+            </p>
+            <p>
+                Throughout this project, I've honed my skills in React routing, prop management, and navigation implementation. Deploying the app to GitHub Pages was a valuable learning experience that enhanced my understanding of project deployment.
+            </p>
+            <p>
+                The captivating animal images that add visual appeal to the app's interface are sourced from{" "}<a href="https://unsplash.com/">Unsplash.</a>
+            </p>
+        </div>
     );
-}
-export default About;
+};
+
+export default AboutPage;
