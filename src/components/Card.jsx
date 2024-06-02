8a569b32556493c6366b50403dffffac741751b5
Dana Popa <popa.dana100@gmail.com>
2024-06-02 21:28:25 +0300

    Add-card-enhancements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,37 @@
 import { Link } from "react-router-dom";
 
-const Card = ({name, likes, addLikes, removeCard, removeLikes}) => {
+const Card = ({ name, likes, addLikes, removeCard, removeLikes }) => {
+    const isLiked = likes >= 5;
+
     return (
-        <div className='card'>
+        <div className="card">
             <h2>{name}</h2>
-            <img src={`https://source.unsplash.com/400x400/?${name}`}/>
+            <img
+                src={`https://source.unsplash.com/400x400/?${name}`}
+                alt={`Photo of ${name}`}
+                className="card-image"
+            />
 
-            <button onClick={addLikes}> 
-            <span className="material-symbols-outlined">heart_plus</span>
+            <div className="likesDisplay">
+                <button onClick={addLikes} className="likeButton">
+                    <span className="material-symbols-outlined">heart_plus</span>
+                </button>
+                <div className="likesMiddle">
+                    <span className={`material-symbols-outlined ${isLiked && "likesRed"}`}>
+                        {likes >= 0 ? "favorite" : "heart_broken"}
+                    </span>{" "}
+                    {likes}
+                </div>
+                <button onClick={removeLikes} className="likeButton">
+                    <span className="material-symbols-outlined">heart_minus</span>
+                </button>
+            </div>
+
+            <button onClick={removeCard} className="removeButton">
+                <span className="material-symbols-outlined">close</span>
             </button>
 
-            <p>{likes}</p>
-
-            <button onClick={removeLikes}>
-            <span className="material-symbols-outlined">heart_minus</span>
-            </button>
-
-            <button onClick={removeCard}>
-            <span className="material-symbols-outlined">cancel</span>
-            </button>
-           <Link to={name}>See More</Link>
+            <Link to={name} className="seeMoreLink">See More</Link>
         </div>
     );
 };
