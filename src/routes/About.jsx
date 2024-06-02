d216397dbc1d35078ee63614094c42db2214da18
Dana Popa <popa.dana100@gmail.com>
2024-06-02 23:22:17 +0300

    Add about page UI

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,9 +3,12 @@ const AboutPage = () => {
         <div id="AboutPage">
             <h2>About</h2>
             <p>
-                In this React application, I've curated a collection of animal profiles. Users can interact by liking or removing animals, and there's a convenient search feature for finding specific ones. It's a comprehensive way to explore various creatures from the comfort of your browser. The animals are neatly categorized, making navigation intuitive. Clicking on any animal reveals detailed information, complemented by a handy back button for easy browsing.
+                In this React application, I've curated a collection of animal profiles. Users can interact by liking or removing animals, and there's a convenient search feature for finding specific ones. <br />It's a comprehensive way to explore various creatures from the comfort of your browser. The animals are neatly categorized, making navigation intuitive. Clicking on any animal reveals detailed information, complemented by a handy back button for easy browsing.
+            </p>
+            <p>
                 What sets this app apart is the integration of Wikipedia excerpts, providing enriching insights into each animal. It's a blend of entertainment and education!
             </p>
+
             <p>
                 Throughout this project, I've honed my skills in React routing, prop management, and navigation implementation. Deploying the app to GitHub Pages was a valuable learning experience that enhanced my understanding of project deployment.
             </p>
