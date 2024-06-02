eccf599f993554c47accbbff8ec3adc87537dd62
Dana Popa <popa.dana100@gmail.com>
2024-06-02 15:24:46 +0300

    Add-dynamic-search-bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-      <header className="header">
-        <Link><h1>Zoo app</h1></Link>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to='/'>Home</NavLink>
-              <NavLink to='/animals'>Animals</NavLink>
-              <NavLink to='/birds'>Birds</NavLink>
-              <NavLink to='/insects'>Insects</NavLink>
-              <NavLink to='/fishes'>Fishes</NavLink>
-              <NavLink to='/about'>About</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  };
-  
-  export default Header;
+  return (
+    <header className="header">
+      <Link><h1>Zoo app</h1></Link>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/animals'>Animals</NavLink>
+            <NavLink to='/birds'>Birds</NavLink>
+            <NavLink to='/insects'>Insects</NavLink>
+            <NavLink to='/fishes'>Fish</NavLink>
+            <NavLink to='/about'>About</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/routes/CategoryPage.jsx
+++ src/routes/CategoryPage.jsx
@@ -1,38 +1,48 @@
 import { useLocation, useParams } from "react-router-dom";
 import Card from "../components/Card";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CategoryPage = ({ removeCard, removeLikes, addLikes, ...rest }) => {
-
     const { category } = useParams();
+    const location = useLocation();
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        setSearch("");
+    }, [location]);
+
+    const searchHandler = (event) => {
+        setSearch(event.target.value);
+    };
 
     const categoryItems = rest[category];
-    const location = useLocation();
-    const [search, setSearch] = useState('');
 
-    const searchHandler = (event)
     return (
         <>
             <h2>{category}</h2>
-            <input
-                type='text'
-                onChange={searchHandler}
-                placeholder="Search"
-                value={search}
-            />
+            <div className="searcharea">
+                <input
+                    type="text"
+                    onChange={searchHandler}
+                    placeholder="Search"
+                    value={search}
+                />
+                <button onClick={() => setSearch("")}>Clear</button>
+            </div>
             {categoryItems
-                .filter((el) => el.name.toLowerCase().includes(search.toLowerCase())).map((item) => {
-                    return (
-                        <Card
-                            key={item.name}
-                            name={item.name}
-                            likes={item.likes}
-                            addLikes={() => addLikes(item.name, category, 'add')}
-                            removeLikes={() => removeLikes(item.name, category, 'remove')} removeCard={() => removeCard(item.name, category)}
-                        />
-                    );
-                })}
+                .filter((el) => el.name.toLowerCase().includes(search.toLowerCase()))
+                .map((item) => (
+                    <Card
+                        key={item.name}
+                        name={item.name}
+                        likes={item.likes}
+                        addLikes={() => addLikes(item.name, category, "add")}
+                        removeLikes={() => removeLikes(item.name, category, "remove")}
+                        removeCard={() => removeCard(item.name, category)}
+                    />
+                ))}
         </>
     );
-}
+};
+
 export default CategoryPage;
